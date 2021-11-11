f9aa2732598f7492aeb0da6b7cca3df91aba9343
yuxiaoyu <pangzidefuji@sina.com>
2021-11-11 15:21:56 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
-    <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    nihao
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
